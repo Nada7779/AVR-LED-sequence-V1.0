082e82b153cf398ce614dd016f88937f29429944
Nada7779 <118090905+Nada7779@users.noreply.github.com>
2023-04-08 11:31:25 +0200

    Add files via upload

---
+++ LED sequence V1.0/GccApplication1/Application/application.c
@@ -0,0 +1,87 @@
+/*
+ * application.h
+ *
+ * Created: 4/8/2023 10:08:38 AM
+ *  Author: Nada
+ */ 
+#include "application.h"
+void APP_init(void)
+{
+	// LEDs init
+	LED_init(led_1_port, led_1_pin);
+	LED_init(led_2_port, led_2_pin);
+	LED_init(led_3_port, led_3_pin);
+	LED_init(led_4_port, led_4_pin);
+	// button init
+	Button_init(BUTTON_1_PORT, BUTTON_1_PIN);
+}
+void APP_start(void)
+	{
+		uint8_t buttonstate= LOW;
+		// Initially, all LEDs are OFF
+		LED_off(led_1_port, led_1_pin);
+		LED_off(led_2_port, led_2_pin);
+		LED_off(led_3_port, led_3_pin);
+		LED_off(led_4_port, led_4_pin);
+		
+		Button_read(BUTTON_1_PORT, BUTTON_1_PIN, &buttonstate);
+		while(buttonstate == LOW)  {Button_read(BUTTON_1_PORT, BUTTON_1_PIN, &buttonstate);}
+		// Once BUTTON1 is pressed, LED1 will be ON
+		// Press 1 (ON, OFF, OFF, OFF)
+		while (buttonstate == HIGH)
+		{
+			LED_on(led_1_port, led_1_pin);
+			Button_read(BUTTON_1_PORT, BUTTON_1_PIN, &buttonstate);
+		}
+		while(buttonstate == LOW) { Button_read(BUTTON_1_PORT, BUTTON_1_PIN, &buttonstate);	 }
+		// Press 2 (ON, ON, OFF, OFF)
+		while (buttonstate == HIGH)
+		{
+			LED_on(led_2_port, led_2_pin);
+			Button_read(BUTTON_1_PORT, BUTTON_1_PIN, &buttonstate);
+		}
+		while(buttonstate == LOW) { Button_read(BUTTON_1_PORT, BUTTON_1_PIN, &buttonstate);	 }
+		// Press 3 (ON, ON, ON, OFF)
+		while (buttonstate == HIGH)
+		{
+			LED_on(led_3_port, led_3_pin);
+			Button_read(BUTTON_1_PORT, BUTTON_1_PIN, &buttonstate);
+		}
+		while(buttonstate == LOW) { Button_read(BUTTON_1_PORT, BUTTON_1_PIN, &buttonstate);	 }
+		// Press 4 (ON, ON, ON, ON)
+		while (buttonstate == HIGH)
+		{
+			LED_on(led_4_port, led_4_pin);
+			Button_read(BUTTON_1_PORT, BUTTON_1_PIN, &buttonstate);
+		}
+		while(buttonstate == LOW) { Button_read(BUTTON_1_PORT, BUTTON_1_PIN, &buttonstate);	 }
+		// At the fifth press, LED1 will changed to be OFF
+		// Press 5 (OFF, ON, ON, ON)
+		while (buttonstate == HIGH)
+		{
+			LED_off(led_1_port, led_1_pin);
+			Button_read(BUTTON_1_PORT, BUTTON_1_PIN, &buttonstate);
+		}
+		while(buttonstate == LOW) { Button_read(BUTTON_1_PORT, BUTTON_1_PIN, &buttonstate);	 }
+		// Press 6 (OFF, OFF, ON, ON)
+		while (buttonstate == HIGH)
+		{
+			LED_off(led_2_port, led_2_pin);
+			Button_read(BUTTON_1_PORT, BUTTON_1_PIN, &buttonstate);
+		}
+		while(buttonstate == LOW) { Button_read(BUTTON_1_PORT, BUTTON_1_PIN, &buttonstate);	 }
+		// Press 7 (OFF, OFF, OFF, ON)
+		while (buttonstate == HIGH)
+		{
+			LED_off(led_3_port, led_3_pin);
+			Button_read(BUTTON_1_PORT, BUTTON_1_PIN, &buttonstate);
+		}
+		while(buttonstate == LOW) { Button_read(BUTTON_1_PORT, BUTTON_1_PIN, &buttonstate);	 }
+		// Press 8 (OFF, OFF, OFF, OFF)
+		while (buttonstate == HIGH)
+		{
+			LED_off(led_4_port, led_4_pin);
+			Button_read(BUTTON_1_PORT, BUTTON_1_PIN, &buttonstate);
+		}
+	}
+
---
+++ LED sequence V1.0/GccApplication1/Debug/Application/application.d
@@ -0,0 +1,20 @@
+Application/application.d Application/application.o: \
+ ../Application/application.c ../Application/application.h \
+ ../Application/../LED/led.h ../Application/../LED/../DIO/DIO.h \
+ ../Application/../LED/../DIO/../register.h \
+ ../Application/../LED/../DIO/../types.h \
+ ../Application/../Button/button.h ../Application/../Button/../DIO/DIO.h
+
+../Application/application.h:
+
+../Application/../LED/led.h:
+
+../Application/../LED/../DIO/DIO.h:
+
+../Application/../LED/../DIO/../register.h:
+
+../Application/../LED/../DIO/../types.h:
+
+../Application/../Button/button.h:
+
+../Application/../Button/../DIO/DIO.h:
